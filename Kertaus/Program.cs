aab7b7a95f390e5ebfcfadd7218c5453413aa95d
Teemu Pussinen <dtepu001@edu.xamk.fi>
2025-02-05 10:48:14 +0200

    added ci/cd

--- Kertaus/Program.cs
+++ Kertaus/Program.cs
@@ -23,12 +23,8 @@ namespace Kertaus
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # OJY-Kertaus
+
+
+![CI Pipeline](https://github.com/dtepu/OJY-Kertaus/actions/workflows/ci.yml/badge.svg)
+![CD Pipeline](https://github.com/dtepu/OJY-Kertaus/actions/workflows/cd.yml/badge.svg)
